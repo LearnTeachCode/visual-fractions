077e2ed20b8271edb6082c2e82be2d4ea63cc309
VincentG123 <vinnyyoga2003@yahoo.com>
2017-01-03 11:25:46 -0800

    created rectangle function

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 // inspiration: https://codepen.io/rachsmith/post/my-process-for-building-generative-pens
 
 var svgns = "http://www.w3.org/2000/svg";
-var rect = document.createElementNS(svgns, 'rect');
-rect.setAttributeNS(null, 'x', 100);
-rect.setAttributeNS(null, 'y', 100);
-rect.setAttributeNS(null, 'height', '200');
-rect.setAttributeNS(null, 'width', '50');
-rect.setAttributeNS(null, 'fill', 'blue');
-document.getElementById('svgOne').appendChild(rect);
+
+var color1 = "pink";
+var color2 = "red";
+var color3 = "green";
+
+var makeRectangle = function(xAxis, yAxis , recHeight, recWidth, color){
+	var rect = document.createElementNS(svgns, 'rect');
+	rect.setAttributeNS(null, 'x', xAxis);
+	rect.setAttributeNS(null, 'y', yAxis);
+	rect.setAttributeNS(null, 'height', recHeight);
+	rect.setAttributeNS(null, 'width', recWidth);
+	rect.setAttributeNS(null, 'fill', color);
+	document.getElementById('svgOne').appendChild(rect);
+};
+
+makeRectangle(200, 300, 100, 50, color1);
+makeRectangle(250, 300, 100, 50, color2);
+makeRectangle(300, 300, 100, 50, color3);
+
