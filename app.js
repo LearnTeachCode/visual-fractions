f1d09cf8f819ce42c5e0f4f99c30c53962a2066d
VincentG123 <vinnyyoga2003@yahoo.com>
2017-01-03 16:39:49 -0800

    created fraction maker

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var color1 = "pink";
 var color2 = "red";
 var color3 = "green";
 
-var makeRectangle = function(xAxis, yAxis , recHeight, recWidth, color){
+var makeRectangle = function(xAxis, yAxis , recWidth, recHeight, color){
 	var rect = document.createElementNS(svgns, 'rect');
 	rect.setAttributeNS(null, 'x', xAxis);
 	rect.setAttributeNS(null, 'y', yAxis);
@@ -16,7 +16,17 @@ var makeRectangle = function(xAxis, yAxis , recHeight, recWidth, color){
 	document.getElementById('svgOne').appendChild(rect);
 };
 
-makeRectangle(200, 300, 100, 50, color1);
-makeRectangle(250, 300, 100, 50, color2);
-makeRectangle(300, 300, 100, 50, color3);
+makeRectangle(200, 300, 100, 100, color1);
+makeRectangle(200, 400, 100, 100, color2);
+makeRectangle(200, 500, 100, 100, color3);
 
+//makeRectangle(400, 300, 50, 420, color1); 
+
+
+var makeFraction = function(num){
+	for (var i = 0; i<num; i++){
+	makeRectangle(400, 300+i*420/num, 50, 420/num, color1);
+	}	
+};
+
+makeFraction(2);
